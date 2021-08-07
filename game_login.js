506fa54d5261d42946028f185a400a7365fed444
Parv-do <82232676+Parv-do@users.noreply.github.com>
2021-08-07 12:20:55 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function Parvsaini(){
+       player1_name = document.getElementById("player1_name").value;
+       player2_nmae = document.getElementById("player2_name").value;
+
+       localStorage.setItem("player1_name",player1_name);
+       localStorage.setItem("player2_name",player2_name);
+
+       window.location.replace("homepage.html");
+}
